1259998ec78cf3bbed6c29e333f4497a69b88415
dk134dk <dk134dk@yandex.com>
2025-09-17 14:13:21 +0800

    Update mk_s905d_n1.sh

--- N1-IMM/mk_s905d_n1.sh
+++ N1-IMM/mk_s905d_n1.sh
@@ -42,12 +42,14 @@ TGT_IMG="${WORK_DIR}/openwrt_${SOC}_${BOARD}_${OPENWRT_VER}_k${KERNEL_VERSION}${
 ###########################################################################
 KMOD="${PWD}/files/kmod"
 KMOD_BLACKLIST="${PWD}/files/kmod_blacklist"
+MAC_SCRIPT1="${PWD}/files/fix_wifi_macaddr.sh"
 MAC_SCRIPT2="${PWD}/files/find_macaddr.pl"
 MAC_SCRIPT3="${PWD}/files/inc_macaddr.pl"
 CPUSTAT_SCRIPT="${PWD}/files/cpustat"
 CPUSTAT_SCRIPT_PY="${PWD}/files/cpustat.py"
 INDEX_PATCH_HOME="${PWD}/files/index.html.patches"
 GETCPU_SCRIPT="${PWD}/files/getcpu"
+TTYD="${PWD}/files/ttyd"
 FLIPPY="${PWD}/files/scripts_deprecated/flippy_cn"
 BANNER="${PWD}/files/banner"
 
@@ -55,6 +57,9 @@ BANNER="${PWD}/files/banner"
 FMW_HOME="${PWD}/files/firmware"
 SMB4_PATCH="${PWD}/files/smb4.11_enable_smb1.patch"
 SYSCTL_CUSTOM_CONF="${PWD}/files/99-custom.conf"
+
+# 20200709 add
+COREMARK="${PWD}/files/coremark.sh"
 
 # 20200930 add
 SND_MOD="${PWD}/files/s905d/snd-meson-gx"
@@ -80,6 +85,9 @@ FIP_HOME="${PWD}/files/meson_btld/with_fip/s905d"
 UBOOT_WITH_FIP="${FIP_HOME}/n1-u-boot.bin.sd.bin"
 UBOOT_WITHOUT_FIP_HOME="${PWD}/files/meson_btld/without_fip"
 UBOOT_WITHOUT_FIP="u-boot-n1.bin"
+
+# 20210208 add
+WIRELESS_CONFIG="${PWD}/files/s905d/wireless"
 
 # 20210307 add
 SS_LIB="${PWD}/files/ss-glibc/lib-glibc.tar.xz"
@@ -127,7 +135,7 @@ check_depends
 
 SKIP_MB=4
 BOOT_MB=256
-ROOTFS_MB=960
+ROOTFS_MB=1650
 SIZE=$((SKIP_MB + BOOT_MB + ROOTFS_MB))
 create_image "$TGT_IMG" "$SIZE"
 create_partition "$TGT_DEV" "msdos" "$SKIP_MB" "$BOOT_MB" "fat32" "0" "-1" "btrfs"
@@ -169,11 +177,16 @@ copy_supplement_files
 extract_glibc_programs
 adjust_docker_config
 adjust_openssl_config
+adjust_qbittorrent_config
 adjust_getty_config
 adjust_samba_config
+adjust_nfs_config "mmcblk2p4"
 adjust_openssh_config
+adjust_openclash_config
 use_xrayplug_replace_v2rayplug
 create_fstab_config
+adjust_turboacc_config
+adjust_ntfs_config
 adjust_mosdns_config
 patch_admin_status_index_html
 adjust_kernel_env
